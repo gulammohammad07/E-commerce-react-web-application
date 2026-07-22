9759a1264dbe3d8c81becfca6310a33a0beb4905
shaaf ansari <shaafansari24@gmail.com>
2026-07-22 15:54:44 +0530

    search funtionality

---
+++ Server/controllers/searchController.js
@@ -0,0 +1,50 @@
+import products from "../data/products.js";
+
+export const globalSearch = (req, res) => {
+  const query = req.query.q?.trim().toLowerCase();
+
+  if (!query) {
+    return res.json({
+      success: true,
+      suggestions: [],
+      products: [],
+      categories: [],
+    });
+  }
+
+  // Products
+  const matchedProducts = products.filter((product) => {
+  const name = product.name?.toLowerCase() || "";
+  const brand = product.brand?.toLowerCase() || "";
+  const category = product.category?.toLowerCase() || "";
+  const description = product.description?.toLowerCase() || "";
+
+  return (
+    name.includes(query) ||
+    brand.includes(query) ||
+    category.includes(query) ||
+    description.includes(query)
+  );
+});
+
+  // Suggestions
+  const suggestions = [
+    ...new Set(
+      matchedProducts.map((item) => item.name)
+    ),
+  ].slice(0, 5);
+
+  // Categories
+  const categories = [
+    ...new Set(
+      matchedProducts.map((item) => item.category)
+    ),
+  ];
+
+  res.json({
+    success: true,
+    suggestions,
+    products: matchedProducts.slice(0, 8),
+    categories,
+  });
+};
